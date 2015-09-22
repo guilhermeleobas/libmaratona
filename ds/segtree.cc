f9950f4e484e9a7693c834c8b990164e99b98c5d
Guilherme Leobas <guilhermeleobas@gmail.com>
2015-09-22 21:45:59 +0100

    update segtree.cc

--- ds/segtree.cc
+++ ds/segtree.cc
@@ -6,41 +6,50 @@ void inicializa (int node, int l, int r){
     if (l > r) return;
 
     if (l == r){
-        segtree[node] = vetor[r];
+      segtree[node] = vetor[r];
     }
     else {
         inicializa (2*node+1, l, (l+r)/2);
         inicializa (2*node+2, (l+r)/2+1, r);
 
-        segtree[node] = max (segtree[2*node+1],segtree[2*node+2]);
+        segtree[node] = segtree[2*node+1] * segtree[2*node+2];
     }
 
 }
 
-void update (int node, int l, int r, int x){
+// default values
+// update (0, 0, size-1, pos);
+// size-1 => size of the array "vetor"
+// pos => position in the array "vetor" that you previously did the change
+void update (int node, int l, int r, int pos){
 
-    if (l > r || l > x || r < x)
+    if (l > r || l > pos || r < pos)
         return;
 
-    if (l == x && r == x){
-        segtree[node] = vetor[l];
+    if (l == pos && r == pos){
+        segtree[node] = vetor[pos];
         return;
     }
 
-    update (2*node+1, l, (l+r)/2, x);
-    update (2*node+2, (l+r)/2+1, r, x);
+    update (2*node+1, l, (l+r)/2, pos);
+    update (2*node+2, (l+r)/2+1, r, pos);
 
     // Condição: pai = max (fllho1, filho2);
-    segtree[node] = max(segtree[2*node+1], segtree[2*node+2]);
+    segtree[node] = segtree[2*node+1] * segtree[2*node+2];
 }
 
+// default value
+// query (0, 0, size-1, i, j)
+// size-1 => reefers to the size of the array vetor
+// [i, j] => query pointers
 int query (int node, int l, int r, int i, int j){
 
     int p1, p2;
 
     //size overflow;
-    if (l > r || l > j || r < i)
-        return 0;
+    if (l > r || l > j || r < i){
+      return 1000000007;;
+    }
 
     //Intervalo incluido
     if (l >= i && r <= j)
@@ -51,5 +60,10 @@ int query (int node, int l, int r, int i, int j){
     p2 = query (2*node+2, (l+r)/2+1, r, i, j);
 
     // Condição: pai = max (fllho1, filho2);
-    return (max(p1, p2));
+    if (p1 == 1000000007)
+      return p2;
+    if (p2 == 1000000007)
+      return p1;
+        
+    return p1*p2;
 }
