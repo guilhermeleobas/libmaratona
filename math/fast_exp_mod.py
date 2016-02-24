0066925cf35a49c085cade013f419735f2f88e6c
Guilherme Leobas <guilhermeleobas@gmail.com>
2016-02-24 22:14:52 +0000

    Create fast_exp_mod.py

---
+++ math/fast_exp_mod.py
@@ -0,0 +1,17 @@
+
+// return (base ˆ exp) % mod;
+
+def exp_mod (base, exp, mod):
+    if (mod == 1):
+        return 0;
+    
+    number = 1;
+    
+    while (exp):
+        if (exp & 1):
+            number = (number * base) % mod;
+        exp >>= 1
+        base = (base * base) % mod;
+    
+    return number;
+
